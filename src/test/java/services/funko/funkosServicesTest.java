ae61ce45bf33e3d85ebd86454ca70773b741b729
Binweiwang <binweiwang1029@gmail.com>
2023-10-09 23:00:44 +0200

    Test AsycFunko

--- src/test/java/services/funko/funkosServicesTest.java
+++ src/test/java/services/funko/funkosServicesTest.java
@@ -32,6 +32,79 @@ class funkosServicesTest {
     FunkoServiceImp funkoServiceImp;
 
 @Test
+void kunkosStitch() throws SQLException {
+    var funkos = of(
+            getFunko(1L,1L, "test-1"),
+            getFunko(2L,2L, "test-2"),
+            getFunko(3L,3L, "test-3")
+    );
+    when(funkoRepository.findAll()).thenReturn(CompletableFuture.completedFuture(new ArrayList<>(funkos)));
+    var result = funkoServiceImp.funkosStitch().join();
+    assertEquals(result.size(), 0);
+}
+@Test
+void numeroFunkoStitch() throws SQLException, FunkoException, ExecutionException, InterruptedException {
+    ArrayList<Funko> funkos = new ArrayList<>();
+    funkos.add(getFunko(1L,1L, "test-1"));
+    funkos.add(getFunko(2L,2L, "test-2"));
+    funkos.add(getFunko(3L,3L, "test-3"));
+    when(funkoRepository.findAll()).thenReturn(CompletableFuture.completedFuture(funkos));
+    var result = funkoServiceImp.numeroFunkosStitch().get();
+    assertEquals(result,0 );
+}
+@Test
+void funkoLanzados2023() throws SQLException, ExecutionException, InterruptedException {
+ArrayList<Funko> funkos = new ArrayList<>();
+    funkos.add(getFunko(1L,1L, "test-1"));
+    funkos.add(getFunko(2L,2L, "test-2"));
+    funkos.add(getFunko(3L,3L, "test-3"));
+    when(funkoRepository.findAll()).thenReturn(CompletableFuture.completedFuture(funkos));
+    var result = funkoServiceImp.funkoLanzados2023().get();
+    assertEquals(result.size(), 3);
+
+}
+@Test
+void numeroFunkoPorModelos() throws SQLException, ExecutionException, InterruptedException, FunkoException {
+    ArrayList<Funko> funkos = new ArrayList<>();
+    funkos.add(getFunko(1L,1L, "test-1"));
+    funkos.add(getFunko(2L,2L, "test-2"));
+    funkos.add(getFunko(3L,3L, "test-3"));
+    when(funkoRepository.findAll()).thenReturn(CompletableFuture.completedFuture(funkos));
+    when(funkoRepository.findByModelo("DISNEY")).thenReturn(CompletableFuture.completedFuture(funkos));
+    var result = funkoServiceImp.numeroFunkoPorModelos().get();
+    assertEquals(result.size(), 1);
+    assertEquals(result.get("DISNEY"), 3);
+}
+@Test
+void agrupadoPorModelos() throws SQLException, FunkoException, ExecutionException, InterruptedException {
+    ArrayList<Funko> funkos = new ArrayList<>();
+    funkos.add(getFunko(1L,1L, "test-1"));
+    funkos.add(getFunko(2L,2L, "test-2"));
+    funkos.add(getFunko(3L,3L, "test-3"));
+    when(funkoRepository.findAll()).thenReturn(CompletableFuture.completedFuture(funkos));
+    when(funkoRepository.findByModelo("DISNEY")).thenReturn(CompletableFuture.completedFuture(funkos)
+    );
+    var result = funkoServiceImp.agrupadoPorModelos().get();
+    assertEquals(result.size(), 1);
+    assertEquals(result.get("DISNEY").size(), 3);
+
+
+}
+
+
+@Test
+void mediaFunko() throws SQLException, ExecutionException, InterruptedException {
+    ArrayList<Funko> funkos = new ArrayList<>();
+    funkos.add(getFunko(1L,1L, "test-1", 9.5));
+    funkos.add(getFunko(2L,2L, "test-2", 8.5));
+    funkos.add(getFunko(3L,3L, "test-3", 10.5));
+    when(funkoRepository.findAll()).thenReturn(CompletableFuture.completedFuture(funkos));
+
+    double media = funkoServiceImp.mediaFunko().get();
+    assertEquals(media, 9.5);
+
+}
+@Test
 void funkoMasCaro() throws ExecutionException, InterruptedException, FunkoException, SQLException {
     ArrayList<Funko> funkos = new ArrayList<>();
     funkos.add(getFunko(1L,1L, "test-1", 9.5));
